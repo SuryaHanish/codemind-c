dcb49a017a078cd14d3edeab6292554556d2ca64
SuryaHanish <129808894+SuryaHanish@users.noreply.github.com>
2023-05-21 10:32:45 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",'A'-1+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-int main(){
-    int  n,i,j;
+int main()
+{
+    int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n;j++){
-            if(i==j)
-            printf("0");
-            else
-            printf("x");
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",'A'-1+i);
         }
         printf("
 ");
