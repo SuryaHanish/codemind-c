bac10f2aa9b3b95e97e0afbb7c24ccb321d17c73
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-05-31 09:03:24 +0530

    #include<stdio.h> int main(){     int t;     scanf("%d", &t);     while(t--){         int a, b;         scanf("%d %d", &a, &b);         printf("%d ", a+b);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-int main()
-{
-    int t,i,a,b;
-    scanf("%d",&t);
-    for(i=0;i<t;i++)
-    {
-        scanf("%d%d",&a,&b);
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int a, b;
+        scanf("%d %d", &a, &b);
         printf("%d
-",a+b);
+", a+b);
     }
 }
