6408f5dd26f36068f4397e5a65f91c2ff302478d
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-05-31 09:01:21 +0530

    #include<stdio.h> int perfect(int num){     int i;     for(i=1;i*i<=num;i++){         if(num%i==0 && num/i==i){             return 1;         }     }     return 0; } int main(){     int n;     scanf("%d", &n);     if(perfect(n)){         printf("True");     }     else{         printf("False");     } }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int perfect(int num){
+    int i;
+    for(i=1;i*i<=num;i++){
+        if(num%i==0 && num/i==i){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    if(perfect(n)){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+}
