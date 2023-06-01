8d95e7059c1b9bf45f565cc304cad285b9aafa15
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-06-01 17:36:03 +0530

    #include<stdio.h> int main(){     int n, i, j, d, a[100];     scanf("%d", &n);     i=0;     while(n!=0){         d=n%10;         n=n/10;         a[i]=d;         i++;     }     for(j=i-1;j>=0;j--){         if(a[j]==6){             a[j]=9;             break;         }     }     for(j=i-1;j>=0;j--){         printf("%d",a[j]);     } }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,28 +1,21 @@
 #include<stdio.h>
-int main()
-{
-    int n,d,a[100],i,j;
-    scanf("%d",&n);
+int main(){
+    int n, i, j, d, a[100];
+    scanf("%d", &n);
     i=0;
-    while(n!=0)
-    {
+    while(n!=0){
         d=n%10;
         n=n/10;
         a[i]=d;
         i++;
     }
-    for(j=i-1;j>=0;j--)
-    {
-        {
-            if(a[j]==6)
-            {
-                a[j]=9;
-                break;
-            }
+    for(j=i-1;j>=0;j--){
+        if(a[j]==6){
+            a[j]=9;
+            break;
         }
     }
-    for(j=i-1;j>=0;j--)
-    {
+    for(j=i-1;j>=0;j--){
         printf("%d",a[j]);
     }
 }
